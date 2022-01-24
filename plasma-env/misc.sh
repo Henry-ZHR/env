a62402ace917549ba01e190a4d24f195d91661cd
Henry-ZHR <henry-zhr@qq.com>
2022-01-24 10:00:25 +0800

    Update plasma-env/misc.sh

--- plasma-env/misc.sh
+++ plasma-env/misc.sh
@@ -1,3 +1,7 @@
 # $HOME/.config/plasma-workspace/env/misc.sh
 
+export PATH=$HOME/.local/bin:$PATH
+
+export ANDROID_SDK_ROOT="/home/zhr/Android/Sdk"
+export CHROME_EXECUTABLE="/usr/bin/chromium"
 export GTK_USE_PORTAL=1
