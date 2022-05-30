87e0f666f823e5332459a48d84b65e86d65967ba
Henry-ZHR <henry-zhr@qq.com>
2022-05-30 21:44:32 +0800

    Remove scripts related to sandbox Just stop writing another Flatpak. I suppose Bubblejail is enough.

--- steam.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-exec sandbox \
-     ~/Documents/Sandbox/Steam \
-     --setenv GDK_SCALE 2 \
-     -- \
-     /usr/bin/steam \
-     "$@"
--- zshrc.zsh
+++ zshrc.zsh
@@ -7,10 +7,9 @@ alias update-p10k="git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlev
 
 alias update-sub="sudo -u clash /home/clash/update-sub.py && sudo systemctl restart clash@clash"
 
-alias baidunetdisk="sandbox ~/Documents/Sandbox/BaiduNetdisk --bind ~/Downloads/BaiduNetdisk/ ~/Downloads -- /usr/bin/baidunetdisk"
-alias malody="sandbox ~/Documents/Sandbox/Malody -- /usr/bin/wine ~/Malody-4.3.7/malody.exe"
-alias wemeet="sandbox ~/Documents/Sandbox/WeMeet -- sh -c '/opt/apps/com.tencent.meeting.deepin/files/run.sh && sleep infinity'"
-alias xes="sandbox ~/Documents/Sandbox/XES --tmpfs ~/.wine/drive_c/users/zhr/AppData/Local/owcrclient/wcropenclassroom/dump/ -- wine 'C:\Program Files (x86)\xeszx\3.21.0.1680\owcrclient.exe'"
+# alias baidunetdisk="sandbox ~/Documents/Sandbox/BaiduNetdisk --bind ~/Downloads/BaiduNetdisk/ ~/Downloads -- /usr/bin/baidunetdisk"
+# alias malody="sandbox ~/Documents/Sandbox/Malody -- /usr/bin/wine ~/Malody-4.3.7/malody.exe"
+# alias xes="sandbox ~/Documents/Sandbox/XES --tmpfs ~/.wine/drive_c/users/zhr/AppData/Local/owcrclient/wcropenclassroom/dump/ -- wine 'C:\\Program Files (x86)\\xeszx\\3.24.0.1755\\owcrclient.exe'"
 
 disable kill
 disable printf
