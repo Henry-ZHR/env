1082c51294f8873fbd2ada750a5d553b5b1206be
Henry-ZHR <henry-zhr@qq.com>
2023-07-19 21:05:36 +0800

    Use `recvuntil`

--- proxy/sing-box/update-config.py
+++ proxy/sing-box/update-config.py
@@ -2,15 +2,16 @@ import os
 import pwd
 import requests
 import subprocess
-import time
+
+import pwn
 
 subprocess.check_call(['global-proxy', 'set', 'serenity'])
-serenity = subprocess.Popen([
+serenity = pwn.process([
     'sudo', '-u', 'serenity', 'serenity', 'run', '-c',
     '/etc/serenity/config.json'
 ])
 try:
-    time.sleep(5)
+    serenity.recvuntil(b'server started at 127.0.0.1:1070')
     config_content = requests.get('http://127.0.0.1:1070/').content
     with open('/etc/sing-box/config.json', 'w') as config_file:
         config_file.write(config_content.decode())
