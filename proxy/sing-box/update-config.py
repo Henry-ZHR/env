2d3782396febbb441891e1748b28b1e13f4541d4
Henry-ZHR <henry-zhr@qq.com>
2024-07-16 16:30:38 +0800

    Use `serenity export`

--- proxy/sing-box/update-config.py
+++ proxy/sing-box/update-config.py
@@ -1,11 +1,10 @@
 import json
 import os
 import pwd
-import requests
 import subprocess
-import tempfile
 
-import pwn
+from subprocess import PIPE, Popen
+from tempfile import TemporaryDirectory
 
 
 def ensure_permission(path: str, user: str, mode: int):
@@ -14,43 +13,40 @@ def ensure_permission(path: str, user: str, mode: int):
     os.chmod(path, mode)
 
 
-serenity_workdir = tempfile.TemporaryDirectory()
+serenity_workdir = TemporaryDirectory()
 ensure_permission(serenity_workdir.name, 'serenity', 0o700)
 subprocess.check_call(['global-proxy', 'enable', 'serenity'])
-serenity = pwn.process([
-    'sudo', '-u', 'serenity', 'serenity', 'run', '-C', '/etc/serenity', '-D',
-    serenity_workdir.name
-])
-serenity_output = b''
-try:
-    serenity_output += serenity.recvuntil(b'serenity started')
-    s_result = json.loads(requests.get('http://127.0.0.1:1070/').content)
-    outbounds = s_result['outbounds'][3:]
-    assert outbounds[0]['tag'].casefold() == 'Default'.casefold()
+serenity = Popen([
+    'sudo', '-u', 'serenity', 'serenity', 'export', 'default', '-C',
+    '/etc/serenity', '-D', serenity_workdir.name
+],
+                 stdout=PIPE,
+                 stderr=PIPE)
+serenity_output, serenity_err = serenity.communicate()
+subprocess.check_call(['global-proxy', 'disable', 'serenity'])
+print('Serenity log:')
+print(serenity_err.decode())
+if serenity.returncode != 0:
+    print('Serenity return code:', serenity.returncode)
+    exit(1)
 
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    with open(os.path.join(script_dir, 'basic-config.json'),
-              'r') as basic_config_file:
-        config_content = json.loads(basic_config_file.read())
-    assert config_content['outbounds'][0]['tag'] == 'proxy'
-    config_content['outbounds'][0]['outbounds'] = outbounds[0]['outbounds']
-    config_content['outbounds'] += outbounds[1:]
-    with open('/etc/sing-box/clash-api-secret.txt', 'r') as secret_file:
-        secret = secret_file.read()
-        config_content['experimental']['clash_api']['secret'] = secret
+s_result = json.loads(serenity_output)
+outbounds = s_result['outbounds'][3:]
+assert outbounds[0]['tag'] == 'default'
 
-    os.remove('/etc/sing-box/config.json.bak')
-    os.rename('/etc/sing-box/config.json', '/etc/sing-box/config.json.bak')
-    with open('/etc/sing-box/config.json', 'w') as config_file:
-        json.dump(config_content, config_file, ensure_ascii=False, indent=4)
-    ensure_permission('/etc/sing-box/config.json', 'sing-box', 0o600)
-    subprocess.check_call(
-        ['sudo', '-u', 'sing-box', 'sing-box', 'check', '-C', '/etc/sing-box'])
-except:
-    raise
-finally:
-    serenity.terminate()
-    serenity_output += serenity.recvall()
-    print('Serenity log:')
-    print(serenity_output.decode())
-    subprocess.check_call(['global-proxy', 'disable', 'serenity'])
+script_dir = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(script_dir, 'basic-config.json'), 'r') as f:
+    config_content = json.loads(f.read())
+assert config_content['outbounds'][0]['tag'] == 'proxy'
+config_content['outbounds'][0]['outbounds'] = outbounds[0]['outbounds']
+config_content['outbounds'] += outbounds[1:]
+with open('/etc/sing-box/clash-api-secret.txt', 'r') as f:
+    config_content['experimental']['clash_api']['secret'] = f.read()
+
+os.remove('/etc/sing-box/config.json.bak')
+os.rename('/etc/sing-box/config.json', '/etc/sing-box/config.json.bak')
+with open('/etc/sing-box/config.json', 'w') as config_file:
+    json.dump(config_content, config_file, ensure_ascii=False, indent=4)
+ensure_permission('/etc/sing-box/config.json', 'sing-box', 0o600)
+subprocess.check_call(
+    ['sudo', '-u', 'sing-box', 'sing-box', 'check', '-C', '/etc/sing-box'])
