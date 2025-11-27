07fb1e6ec29fbe58021674837bbc53e5e54006f8
Henry-ZHR <henry-zhr@qq.com>
2025-11-27 16:15:00 +0800

    fish: add initial config

---
+++ config.fish
@@ -0,0 +1,73 @@
+# ~/.config/fish/config.fish
+
+function fish_prompt --description 'Informative + Informative Vcs'
+    # Save the return status of the previous command
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+    set -l color_cwd $fish_color_cwd
+    set -l suffix '$'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        set suffix '#'
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        end
+    end
+
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color --bold $fish_color_status)
+    set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+    if not set -q __fish_git_prompt_show_informative_status
+        set -g __fish_git_prompt_show_informative_status 1
+    end
+    if not set -q __fish_git_prompt_hide_untrackedfiles
+        set -g __fish_git_prompt_hide_untrackedfiles 1
+    end
+    if not set -q __fish_git_prompt_color_branch
+        set -g __fish_git_prompt_color_branch magenta --bold
+    end
+    if not set -q __fish_git_prompt_showupstream
+        set -g __fish_git_prompt_showupstream informative
+    end
+    if not set -q __fish_git_prompt_color_dirtystate
+        set -g __fish_git_prompt_color_dirtystate blue
+    end
+    if not set -q __fish_git_prompt_color_stagedstate
+        set -g __fish_git_prompt_color_stagedstate yellow
+    end
+    if not set -q __fish_git_prompt_color_invalidstate
+        set -g __fish_git_prompt_color_invalidstate red
+    end
+    if not set -q __fish_git_prompt_color_untrackedfiles
+        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    end
+    if not set -q __fish_git_prompt_color_cleanstate
+        set -g __fish_git_prompt_color_cleanstate green --bold
+    end
+
+    printf '[%s] %s%s@%s %s%s' \
+        (date "+%H:%M:%S") \
+        (set_color brblue) $USER (prompt_hostname) \
+        (set_color $color_cwd) (prompt_pwd --dir-length=0)
+    if test -n "$pipestatus_string"
+        printf ' %s' $pipestatus_string
+    end
+    set_color normal
+    fish_vcs_prompt
+    printf '\n%s ' $suffix
+end
+
+function push-lineageos-update --wraps='~/Repos/lineage-nio/packages/apps/Updater/push-update.sh'
+    ~/Repos/lineage-nio/packages/apps/Updater/push-update.sh $argv
+end
+
+function update-proxy-config --wraps='sudo python ~/Repos/env/proxy/update-config.py'
+    sudo python ~/Repos/env/proxy/update-config.py $argv
+end
+
+set -x http_proxy http://127.0.0.1:1080
+set -x https_proxy http://127.0.0.1:1080
+
+. /etc/modules/init/fish
+. /etc/modules/init/fish_completion
+ml use "$HOME/.local/share/modulefiles"
